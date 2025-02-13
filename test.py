92780faf163d137ee0a65156c3c2b9d80d0df1ee
kimEHwoo <kimtaewoo721@gmail.com>
2025-02-13 17:35:09 +0900

    BasicProgram

---
+++ test.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_fear_and_greed_index():
+    url = "https://api.alternative.me/fng/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response. json()
+        return data['data' ] [0]
+    else:
+        print(f"Failed to fetch Fear and Greed Index. Status code: {response.status_code}")
+        return None
+
+print(get_fear_and_greed_index())
